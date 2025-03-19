d3b6645a82ae2bb3d70adc6e07c8279ba28ba2d8
LRboice <lucasrboice@gmail.com>
2025-03-18 21:40:10 -0500

    -What about this

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-app/',
+  base: '/personal-site/',
   plugins: [react()],
   define: {
     'process.env': {
